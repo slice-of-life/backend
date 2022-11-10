8431cd1b28d5ede69171c2d73cd71d64fee924d1
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2022-11-10 14:02:03 -0800

    add example code for env tests

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+"""
+    :module_name: __init__
+    :module_summary: slice of life backend initialization
+    :module_author: Nathan Mendoza (nathancm@uci.edu)
+"""
+
+from .api import gimme_five
---
+++ src/api/__init__.py
@@ -0,0 +1,7 @@
+"""
+    :module_name: __init__
+    :module_summary: slice of life api initialization
+    :module_author: Nathan Mendoza (nathancm@uci.edu)
+"""
+
+from .get_five import gimme_five
