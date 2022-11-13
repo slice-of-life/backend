7d5bfe651a22b9e1beb795ac233c904efa89fd83
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2022-11-13 13:28:23 -0800

    added minimal flask application

---
+++ src/sliceoflife_webservice/__init__.py
@@ -0,0 +1,13 @@
+"""
+    :module_name: sliceoflife_webservice
+    :module_summary: entry point for the slice of life backend
+    :module_author: Nathan Mendoza (nathancm@uci.edu)
+"""
+from flask import Flask
+
+from .api import hello
+
+app = Flask(__name__)
+
+app.add_url_rule('/', endpoint='index', view_func=hello)
+
---
+++ src/sliceoflife_webservice/api/routes.py
@@ -0,0 +1,8 @@
+"""
+    :module_name: routes
+    :module_summary: functions that defines the api routes for the slice of life api
+    :module_author: Nathan Mendoza (nathancm@uci.edu)
+"""
+
+def hello():
+    return "<p> Hello, World </p>"
