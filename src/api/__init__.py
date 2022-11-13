d702a7497b142d6fd5ab7ad6feef069660ed6ccc
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2022-11-13 12:59:11 -0800

    remove example code

--- src/api/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-    :module_name: __init__
-    :module_summary: slice of life api initialization
-    :module_author: Nathan Mendoza (nathancm@uci.edu)
-"""
-
-from .get_five import gimme_five
