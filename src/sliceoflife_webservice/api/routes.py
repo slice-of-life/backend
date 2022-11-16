9a8f53ff7cf971f58669cfcaad2de3aea970d27d
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2022-11-16 01:01:15 -0800

    address linting errors

--- src/sliceoflife_webservice/api/routes.py
+++ src/sliceoflife_webservice/api/routes.py
@@ -8,7 +8,12 @@ import logging
 
 LOGGER = logging.getLogger('gunicorn.error')
 
-def hello():
+def hello() -> dict:
+    """
+        A simple GET API route that introduces the Slice Of Life API
+        :returns: A static JSON response
+        :rtype: dict
+    """
     response = {
         'msg': 'Welcome to the first endpoint of the slice of life api'
     }
