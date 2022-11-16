28b476d8e20619bcf00b04ce549e43c95ec55ece
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2022-11-16 15:00:59 -0800

    Logging now fully utilizes gunicorn logging facilities instead

--- src/sliceoflife_webservice/__init__.py
+++ src/sliceoflife_webservice/__init__.py
@@ -14,19 +14,14 @@ LOGGER = logging.getLogger('gunicorn.error')
 
 app = Flask(__name__)
 
-if __name__ != "__main__": #This is weird, but it is definitely suppose to be like this
-    app.logger.handlers = LOGGER.handlers
-    app.logger.setLevel(LOGGER.level)
+LOGGER.info("Created an API application instance")
 
-app.logger.info("Created an API application instance")
-
-app.logger.info("Added the route: GET /api/v1/greeting")
+LOGGER.info("Added the route: GET /api/v1/greeting")
 @app.route('/api/v1/greeting', methods=['GET'])
 def greeting():
     """
        The first API method of the Slice Of Life API
     """
-    app.logger.info("Responding to GET /api/v1/greeting")
-    app.logger.info("Sent: %s", str(hello()))
+    LOGGER.info("Responding to GET /api/v1/greeting")
 
     return hello()
