52e0afda4226b648528702b685271ec5f43b1f49
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2022-11-15 21:29:36 -0800

    add top-level exception declarations

---
+++ src/sliceoflife_webservice/exceptions.py
@@ -0,0 +1,10 @@
+"""
+    :module_name: exceptions
+    :module_summary: exception classes for the sliceoflife API
+    :module_author: Nathan Mendoza (nathancm@uci.edu)
+"""
+
+class SliceOfLifeBaseException(BaseException):
+    """
+        Base exception class for all Slice Of Life API related exceptions
+    """
