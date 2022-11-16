36e4cb5179db44a6a8c60c79982ec3b7c8e4d414
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2022-11-15 23:02:49 -0800

    add logging facilities

--- src/sliceoflife_webservice/__init__.py
+++ src/sliceoflife_webservice/__init__.py
@@ -3,9 +3,22 @@
     :module_summary: entry point for the slice of life backend
     :module_author: Nathan Mendoza (nathancm@uci.edu)
 """
+
+import logging
+
 from flask import Flask
 
 from .api import hello
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+HANDLER = logging.StreamHandler()
+HANDLER.setFormatter(
+    logging.Formatter("[%(asctime)s]:[%(levelname)s] -- %(message)s")
+)
+LOGGER.addHandler(
+    HANDLER
+)
 
 def app():
     """
@@ -14,6 +27,8 @@ def app():
         :rtype: FlaskApp
     """
     api = Flask(__name__)
-    api.add_url_rule('/api/v1/greet', endpoint='index', view_func=hello)
+    LOGGER.info("Creating an api instance")
+    api.add_url_rule('/api/v1/greet', endpoint='greet', view_func=hello)
+    LOGGER.info("Added endpoint: 'GET /api/v1/greet'")
 
     return api
