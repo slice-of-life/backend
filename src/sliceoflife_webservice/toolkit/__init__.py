e3be39ea37821c8dead3649b3b143ee440195065
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2022-11-18 23:00:06 -0800

    add space index class for interacting with application CDN

---
+++ src/sliceoflife_webservice/toolkit/__init__.py
@@ -0,0 +1,7 @@
+"""
+    :module_name: toolkit
+    :module_summary: utilities for communicating with the sliceoflife
+    :module_author: Nathan Mendoza (nathancm@uci.edu)
+"""
+
+from .spaces import SpaceIndex
