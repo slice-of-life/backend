7150d8c05fec1dc007040523c3e476846564362c
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2022-12-09 10:33:03 -0800

    Add decorator functions for uniform error handling API responses

--- src/sliceoflife_webservice/api/__init__.py
+++ src/sliceoflife_webservice/api/__init__.py
@@ -11,6 +11,7 @@ from dotenv import dotenv_values
 
 from ..dbtools import Instance
 from ..toolkit import SpaceIndex
+from ..exceptions import SliceOfLifeAPIException
 
 LOGGER = logging.getLogger("gunicorn.error")
 
@@ -45,3 +46,19 @@ class BaseSliceOfLifeApiResponse(ABC):
 
         LOGGER.debug("Returning shared cdn session")
         return BaseSliceOfLifeApiResponse.blobinstance
+
+def safe_api_callback(method: callable) -> callable:
+    """
+        A decorator that provides error handling for API callbacks
+        :arg method: the API callback to protect
+        :returns: safe API response callback
+        :rtype: callable
+    """
+    def wrapper(ref, *args):
+        try:
+            return method(ref, *args)
+        except SliceOfLifeAPIException as exc:
+            LOGGER.error("Error occurred during execution: %s", str(exc))
+            return ("Internal Server Error", 500)
+
+    return wrapper
