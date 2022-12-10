61901561b8b4f889efe24bce7de4b50542f5667d
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2022-12-09 16:15:20 -0800

    Redefine exception hierarchy for package

--- src/sliceoflife_webservice/exceptions.py
+++ src/sliceoflife_webservice/exceptions.py
@@ -4,42 +4,22 @@
     :module_author: Nathan Mendoza (nathancm@uci.edu)
 """
 
-class SliceOfLifeBaseException(BaseException):
-    """
-        Base exception class for all Slice Of Life API related exceptions
-    """
-
-class DatabaseNotConnectedError(SliceOfLifeBaseException):
-    """
-        Connection thrown if a sql query is attempted when the API has no active database connection
-    """
-
-class SliceOfLifeAPIException(SliceOfLifeBaseException):
+class SliceOfLifeAPIException(Exception):
     """
         Exception thrown when an API request cannot be completed succesfully
     """
 
-class ContentNotRetrievableError(SliceOfLifeBaseException):
+class ContentNotFoundError(SliceOfLifeAPIException):
     """
-        Exception thrown when the CDN cannot be reached
-    """
-
-class DuplicateHandleError(SliceOfLifeBaseException):
-    """
-        Exception thrown when a user is attempted to be created with a handle already in use
+        Exception thrown when an API request cannot find the data it expects
     """
 
-class NoSuchUserError(SliceOfLifeAPIException):
+class AuthorizationError(SliceOfLifeAPIException):
     """
-        Exception thrown when authentication is attempted with a nonexistent handle
-    """
-
-class InvalidCredentialsError(SliceOfLifeAPIException):
-    """
-        Exception thrown whan authenticating with invalid credentials
+        Exception thrown when an API request fails due to insufficient permissions
     """
 
-class NotAuthorizedError(SliceOfLifeAPIException):
+class ServiceNotReachable(SliceOfLifeAPIException):
     """
-        Exception thown when a user is not authorized to perform an action
+        Exception thrown when an API request fails becuase an external service is unreachable
     """
