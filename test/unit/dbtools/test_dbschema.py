da7f63f4109e98c72b4c15f70156943cb4bb8de2
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2022-12-13 14:35:15 -0800

    Full coverage on dbtools.schema submodule

--- test/unit/dbtools/test_dbschema.py
+++ test/unit/dbtools/test_dbschema.py
@@ -4,10 +4,13 @@
     module_author: Nathan Mendoza (nathancm@uci.edu)
 """
 
+import datetime
+
 import pytest
 
 from sliceoflife_webservice.dbtools.schema import User, Task, Post, \
-                                                  Reaction, Completion, Comment
+                                                  Reaction, Completion, Comment, \
+                                                  interpret_as
 
 
 @pytest.mark.parametrize("datatype, exists", [
@@ -22,3 +25,29 @@ from sliceoflife_webservice.dbtools.schema import User, Task, Post, \
 def test_data_schema_is_defined(datatype, exists):
     """Test that the dataclasses defining app dataschema are defined"""
     assert (datatype in globals()) is exists
+
+@pytest.mark.parametrize("datatype, datarow", [
+    (User, ('handle', 'pass', 'email', 'salt', 'first', 'last', 'profile pic')),
+    (Task, (1, 'title', 'description', True)),
+    (Post, (1, 'free text', 'image', datetime.datetime.utcnow(), 'user', 1)),
+    (Reaction, (1, 'emoji', 'user', 1)),
+    (Completion, ('user', 1)),
+    (Comment, (1, datetime.datetime.utcnow(), 'free text', None, 'user', 1))
+])
+def test_data_interpretation(datatype, datarow):
+    """Test that the interpret_as function convert a data tuple to the specified type"""
+    datainstance = interpret_as(datatype, datarow)
+    assert isinstance(datainstance, datatype)
+
+@pytest.mark.parametrize("datatype, datarow", [
+    (Comment, ('handle', 'pass', 'email', 'salt', 'first', 'last', 'profile pic')),
+    (User, (1, 'title', 'description', True)),
+    (Task, (1, 'free text', 'image', datetime.datetime.utcnow(), 'user', 1)),
+    (Post, (1, 'emoji', 'user', 1)),
+    (Reaction, ('user', 1)),
+    (Completion, (1, datetime.datetime.utcnow(), 'free text', None, 'user', 1))
+])
+def test_data_interpretation_misuse(datatype, datarow):
+    """Test that misusing interpret_as throws a TypeError"""
+    with pytest.raises(TypeError):
+        interpret_as(datatype, datarow)
